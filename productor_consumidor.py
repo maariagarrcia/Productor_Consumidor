572113fbec092122ee391ca1d76fa3f42a830ed9
maariagarrcia <mrrmn@myuax.com>
2023-04-17 11:04:53 +0200

    productor

--- productor_consumidor.py
+++ productor_consumidor.py
@@ -35,23 +35,58 @@
 #                                 - crear procesos 
 #  
 
+
+from multiprocessing import Process, Queue
+from colorama import Fore
+import time
+import random
+
+### DPS PASARLO A POO Y A VISTA CONTROLADOR
+
+def arrancar_todo(procesos) -> None:
+    print("* Arrancando procesos...")
+    for p in procesos:
+        p.start()
+
+def cocinar(id_cocinero: int, nombre_comida: str, cola: Queue) -> None:
+    # Contador de platos producidos
+    num_plato = 0
+
+    while True:
+        # Cocinar -> Simular tiempo cocinado
+        time.sleep(random.random())
+
+        # Si no está llena la cola dejar plato
+        # Si la cola esta llena se espera a que algun cliente
+        # consuma algún plato.
+        # Si timeout se supera el cocinero se va a su casa ya
+        # que los clientes han dejado de comer
+
+        try:
+            cola.put([id_cocinero, num_plato, nombre_comida],
+                     block=True, timeout=1)
+            num_plato += 1
+            print(Fore.YELLOW+"+ Cocinero"+Fore.WHITE, id_cocinero, "cocinando", nombre_comida)
+        except:
+            print(Fore.RED+"+ Cocinero", id_cocinero,
+                  "No me necesitan más!!!", num_plato, "<----------"+Fore.WHITE)
+            return
+
+def comer():
+    pass
+
 def crear_cocineros():
     pass
 
 def crear_comensales():
     pass
 
-def cocinar():
-    pass
 
-def comer():
-    pass
-
-def arrancar_todo():
-    pass
-
-def esperar_finalizacion_procesos():
-    pass
+def esperar_finalizacion_procesos(procesos):
+    print("* Esperando finalización procesos...")
+    print("·")
+    for p in procesos:
+        p.join()
 
 def main():
     pass
